6502708239e37f93d5b410ec2d6e2b77e84d81cb
Jeong eun <lisasje25@gmail.com>
2023-02-27 19:35:46 +0900

    [#57] DelegatingPasswordEncoder로 암호화하여 저장, 비교하는 기본 로직 구현

--- src/main/java/com/oti/team2/HomeController.java
+++ src/main/java/com/oti/team2/HomeController.java
@@ -5,10 +5,13 @@ import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.oti.team2.util.springsequrity.MyPasswordEncoder;
 
 import lombok.extern.log4j.Log4j2;
 
@@ -34,7 +37,13 @@ public class HomeController {
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		//
+		String prepw = "1234";
+
+		PasswordEncoder passwordEncoder = MyPasswordEncoder.createDelegatingPasswordEncoder();
+		String postpw = passwordEncoder.encode(prepw);
+		boolean match = passwordEncoder.matches(prepw, postpw);
+		log.info(postpw);
+		log.info(match);
 		return "member/my-todo";
 	}
 
